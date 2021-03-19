e7122679bd5539a9fad46a93b8b04d0497e0b53a
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-19 14:52:40 +0900

    d

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,8 @@ const routes = [
   {
     path: '/member',
     name: 'Member',
-    component: Member
+    component: Member,
+    props:true,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
