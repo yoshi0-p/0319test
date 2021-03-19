ff67c67b161a7caeadcbe3227f2add0931ec9534
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-19 14:33:40 +0900

    first-commit

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <router-link :to= "{ name:'Member' , params :{id:1}}">
+          1へ進む
+        </router-link>
+      </li>
+      <li>
+        <router-link :to= "{ name:'Member' , params :{id:2}}">
+          2へ進む
+        </router-link>
+      </li>
+      <li>
+        <router-link :to= "{ name:'Member' , params :{id:3}}">
+          3へ進む
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<style scoped>
+ul{
+  padding:0px;
+}
+li{
+  list-style:none;
+}
+</style>
---
+++ src/views/Member.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/member">About</router-link>
+    <p>{{id}}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: ["id"],
+}
+</script>
+
+
+<style scoped>
+p{
+  text-align:center;
+}
+</style>
