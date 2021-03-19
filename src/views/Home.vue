5520ea52e37b495b17f47932611449fed1e43cb3
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-19 14:49:58 +0900

    d

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,17 @@
   <div>
     <ul>
       <li>
-        <router-link :to= "{ name:'Member' , params :{id:1}}">
+        <router-link :to= "{ name:'Member' , params :{id: 1 }}">
           1へ進む
         </router-link>
       </li>
       <li>
-        <router-link :to= "{ name:'Member' , params :{id:2}}">
+        <router-link :to= "{ name:'Member' , params :{id: 2 }}">
           2へ進む
         </router-link>
       </li>
       <li>
-        <router-link :to= "{ name:'Member' , params :{id:3}}">
+        <router-link :to= "{ name:'Member' , params :{id: 3 }}">
           3へ進む
         </router-link>
       </li>
